0fac715a90c54a61ec5e73c6615352aa2c4cdd86
namnguyen0917 <nam60946@gmai.com>
2023-04-04 23:37:52 +0700

    cmt 0404

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
-  return (
-    <div className="App">
-        <h1>index 2</h1>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <h1>index 2</h1>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
